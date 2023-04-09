7ec5e0bd8e89faa69cf4cf7f8beeac7abab968bb
Disooloo <Shantsev@internet.ru>
2023-04-09 21:28:10 +0500

    Task1

---
+++ synergyLessonTask/synergyLessonTask2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("synergyLessonTask2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("synergyLessonTask2")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("6a9c96e7-5b93-4373-9bb1-c68872a2ad99")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ synergyLessonTask/synergyLessonTask2/Task1/Task1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace synergyLessonTask2.Task1
+{
+    public class Task1
+    {
+        public static void GetTask1()
+        {
+            #region Тема 1.Создание и использование методов
+
+            #endregion
+
+            int[] numbers1 = { 1, 2, 3 };
+            int[] numbers2 = { 1, 2, 3 };
+            int[] numbers3 = { 1, 2, 3 };
+
+            string[] name = { "Dima", "Sasha" };
+
+            GetNum1(numbers1);
+            GetNum1(name);
+
+
+            int abs1 = Abs(22);
+            int abs2 = Abs(-2);
+
+            Console.WriteLine($"{abs1} | {abs2}");
+
+        }
+
+        private static void GetNum1(int[] array)
+        {
+            foreach(int item in array)
+                Console.WriteLine(item);
+        }
+
+        private static void GetNum1(string[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static int Abs(int value) => value >= 0 ? value : -value;
+    }
+}
